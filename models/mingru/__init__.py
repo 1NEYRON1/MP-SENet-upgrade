45299af8d15b7628fe2c7b6683f27bb52433c91a
Dan <danila-masterok@mail.ru>
2026-02-12 14:28:30 +0300

    upd

--- models/mingru/__init__.py
+++ models/mingru/__init__.py
@@ -1,33 +1,7 @@
 from .min_gru import MinGRU
-from .gtcrn_mingru import (
-    MinGTCRN,
-    MinTRA,
-    MinGRNN,
-    MinDPGRNN,
-    MinGTConvBlock,
-    MinEncoder,
-    MinDecoder,
-)
-from .mpnet_mingru import (
-    MinMPNet,
-    MinFFN,
-    MinTransformerBlock,
-    MinTSTransformerBlock,
-)
+from .mpnet_mingru import MinMPNet
 
 __all__ = [
     "MinGRU",
-    # MinGTCRN
-    "MinGTCRN",
-    "MinTRA",
-    "MinGRNN",
-    "MinDPGRNN",
-    "MinGTConvBlock",
-    "MinEncoder",
-    "MinDecoder",
-    # MinMPNet
     "MinMPNet",
-    "MinFFN",
-    "MinTransformerBlock",
-    "MinTSTransformerBlock",
 ]
