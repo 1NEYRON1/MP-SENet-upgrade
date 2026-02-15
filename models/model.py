42b24a73e344208e9e484b1bf8364a599670876d
Золина Ксения Сергеевна <xenz5240@gmail.com>
2026-02-15 23:54:33 +0300

    initial commit

--- models/model.py
+++ models/model.py
@@ -159,6 +159,46 @@ class MPNet(nn.Module):
                                     denoised_amp*torch.sin(denoised_pha)), dim=-1)
 
         return denoised_amp, denoised_pha, denoised_com
+    
+    
+class MPNet_waveform(nn.Module):
+    def __init__(self, h):
+        super(MPNet_waveform, self).__init__()
+        self.h = h
+        self.num_tscblocks = h.num_tsblocks
+        self.dense_encoder = DenseEncoder(h, in_channel=4)
+
+        self.TSBlocks = nn.ModuleList([])
+        for i in range(h.num_tsblocks):
+            if h.block_type.lower() == "xlstm":
+                self.TSBlocks.append(TFxLSTMBlock(h))
+            elif h.block_type.lower() == "mamba":
+                self.TSBlocks.append(TFMambaBlock(h))
+            elif h.block_type.lower() == "mixed":
+                self.TSBlocks.append(TFMixedBlock(h))
+            else:
+                self.TSBlocks.append(TSTransformerBlock(h))
+        
+        self.mask_decoder = MaskDecoder(h, out_channel=1)
+        self.phase_decoder = PhaseDecoder(h, out_channel=1)
+
+    def forward(self, noisy_amp, noisy_pha, noisy_com): # [B, F, T]
+        
+        noisy_real = noisy_com[..., 0]
+        noisy_imag = noisy_com[..., 1]
+
+        x = torch.stack((noisy_amp, noisy_pha, noisy_real, noisy_imag), dim=-1).permute(0, 3, 2, 1) # [B, 4, T, F]
+        x = self.dense_encoder(x)
+
+        for i in range(self.num_tscblocks):
+            x = self.TSBlocks[i](x)
+        
+        denoised_amp = noisy_amp * self.mask_decoder(x)
+        denoised_pha = self.phase_decoder(x)
+        denoised_com = torch.stack((denoised_amp*torch.cos(denoised_pha),
+                                    denoised_amp*torch.sin(denoised_pha)), dim=-1)
+
+        return denoised_amp, denoised_pha, denoised_com
 
 
 def phase_losses(phase_r, phase_g):
